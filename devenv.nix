8c0aaf64f6c2fc7a8761c186fb8a31bd0a53923b
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-03-21 14:01:13 +0800

    feat: setup devenv

---
+++ devenv.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/packages/
+  packages = [ ];
+
+  # https://devenv.sh/languages/
+  languages = {
+    nix.enable = true;
+  };
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    python3 --version
+  '';
+
+  # https://devenv.sh/git-hooks/
+  git-hooks.hooks = {
+    nixfmt.enable = true;
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
